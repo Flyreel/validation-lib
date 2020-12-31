0d7e37b3b4bd5edd8d9af54e11e1694eec824f6d
Ahmed Alahmady <aalahmady@gmail.com>
2020-12-31 16:09:38 -0600

    export all of constants

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,5 @@
-export {
-  CA_POSTAL_CODE_REGEX,
-  CA_PROVINCES,
-  CA_PROVINCES_ABBR,
-  CA_PROVINCES_FULL,
-  URL_REGEX,
-  US_POSTAL_CODE_REGEX,
-  US_STATES,
-  US_STATES_ABBR,
-  US_STATES_FULL
-} from './constants'
-
+export * from './constants'
 export * from './schemas'
-export * from './validators'
-export * from './utils'
 export * from './types'
+export * from './utils'
+export * from './validators'
