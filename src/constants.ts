99a29c9742ebef7b94ef2f5bf414e9044d9dc632
Ahmed Alahmady <aalahmady@gmail.com>
2021-04-08 22:38:45 -0500

    add canadian territories to CA_PROVINCES

--- src/constants.ts
+++ src/constants.ts
@@ -92,16 +92,19 @@ export const US_STATES = [
 ] as const
 
 export const CA_PROVINCES = [
-  { value: 'ON', label: 'Ontario' },
-  { value: 'QC', label: 'Quebec' },
-  { value: 'NS', label: 'Nova Scotia' },
-  { value: 'NB', label: 'New Brunswick' },
-  { value: 'MB', label: 'Manitoba' },
-  { value: 'BC', label: 'British Columbia' },
-  { value: 'PE', label: 'Prince Edward Island' },
-  { value: 'SK', label: 'Saskatchewan' },
   { value: 'AB', label: 'Alberta' },
-  { value: 'NL', label: 'Newfoundland and Labrador' }
+  { value: 'BC', label: 'British Columbia' },
+  { value: 'MB', label: 'Manitoba' },
+  { value: 'NB', label: 'New Brunswick' },
+  { value: 'NL', label: 'Newfoundland and Labrador' },
+  { value: 'NS', label: 'Nova Scotia' },
+  { value: 'NT', label: 'Northwest Territories' },
+  { value: 'NU', label: 'Nunavut' },
+  { value: 'ON', label: 'Ontario' },
+  { value: 'PE', label: 'Prince Edward Island' },
+  { value: 'QC', label: 'Quebec' },
+  { value: 'SK', label: 'Saskatchewan' },
+  { value: 'YT', label: 'Yukon' }
 ] as const
 
 export const VALID_COUNTRIES = [ValidCountry.US, ValidCountry.CA]
